e5d9da9f97058aafe8bf6f0213f1c32b4149dcf3
GeeTwentyFive <59966523+GeeTwentyFive@users.noreply.github.com>
2022-08-22 15:28:23 +0200

    Delete Common directory

--- Common/Common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "MemoryUtil.h"
-#include "PageUnprotector.h"
-#include "StringUtil.h"
-#include "Stream.h"
-#include "FileStream.h"
-#include "Path.h"
-#include "Directory.h"
--- Common/Directory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Directory
-{
-public:
-    static void                             Create              (const std::wstring& folderPath);
-    static std::vector<std::wstring>        GetFiles            (const std::wstring& folderPath, const std::wstring& pattern);
-    static std::wstring                     GetTempDirectory    ();
-};
