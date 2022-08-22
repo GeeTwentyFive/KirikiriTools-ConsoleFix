7686c18673c2f83476aa53ea42a14a505405b5b3
GeeTwentyFive <59966523+GeeTwentyFive@users.noreply.github.com>
2022-08-22 15:29:13 +0200

    Add files via upload

---
+++ Xp3Pack/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Arc.Ddsi.Xp3Pack
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Xp3Pack folder");
+                return;
+            }
+
+            string folderPath = args[0];
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Specified folder does not exist.");
+                return;
+            }
+
+            int end = 0;
+            string parentDir = "";
+            for (int i = folderPath.Length - 1; i >= 0; --i)
+            {
+                if (folderPath[i].Equals("\\") || folderPath[i].Equals("/"))
+                {
+                    end = i;
+                }
+            }
+            for (int i = 0; i < end; ++i)
+            {
+                parentDir += folderPath[i];
+            }
+
+            string xp3FilePath = parentDir + "patch.xp3";
+            Xp3ArchiveWriter.Write(folderPath, xp3FilePath);
+        }
+    }
+}
